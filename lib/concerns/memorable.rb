1f1849e36c8f56c8aff8739b65d93cc686b525b6
github username <github email address>
2017-10-13 10:31:05 -0400

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,28 @@
+# require_relative '../lib/concerns/memorable'
+# module Memorable
+#   def self.reset_all
+#     @@songs.clear
+#   end
+#   def self.count
+#     self.all.count
+#   end
+# end
+
+
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
